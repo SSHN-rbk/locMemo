04947d6588312436ca734e343a067a1b3ea8cfd3
Hasn <hasnar@windowslive.com>
2017-03-17 14:16:49 +0200

    (feat)addSplashPage,addAccessTokenFromFBLoginToDB,modifyCamHomeStyle

--- client/index.android.js
+++ client/index.android.js
@@ -1,26 +1,3 @@
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-import { Router, Scene } from 'react-native-router-flux';
+'use strict';
 
-import camHomePage from './app/camHomePage';
-import friendMessage from './app/friendMessage';
-
-export default class locMemo extends Component {
-  render() {
-    return (
-      <Router>
-        <Scene key="root">
-          <Scene key="camHomePage" component={camHomePage} title="camHomePage" initial={true} />
-          <Scene key="friendMessage" component={friendMessage} title="friendMessage" />
-        </Scene>
-      </Router>
-    )
-  }
-}
-
-AppRegistry.registerComponent('locMemo', () => locMemo);
+require('./App.js')
---
+++ client/src/firebase/config.js
@@ -0,0 +1,9 @@
+'use strict'
+
+module.exports = {
+  API_KEY: "AIzaSyC0SrkXiPruVpvsCBXV0Z5-thtKOC20U1E",
+  AUTH_DOMAIN: "https://awsomproject-7ab1b.firebaseio.com",
+  DATABASE_URL: "https://awsomproject-7ab1b.firebaseio.com/",
+  STORAGE_BUCKET: "gs://awsomproject-7ab1b.appspot.com",
+  MESSAGING_SENDER_ID: "485229835283"
+}
